d09d9e8c9612af09013c1cfa23c1f38ec84d6059
emant <joshua999x@gmail.com>
2020-09-05 22:54:01 +0200

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json({ status: "ok" });
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' });
 });
 
 const PORT = process.env.PORT || 3000;
