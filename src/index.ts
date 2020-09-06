578e0d0f7ab2c7b7002a97b98e6a924d0b33b554
emant <joshua999x@gmail.com>
2020-09-06 22:50:43 +0200

    add various middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import express from 'express';
+
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
 
 import { httpLogger } from './logger';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(httpLogger());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(compression());
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok' });
